86529815eddc8e3b2a0e06dce9de29d9649cb675
Karishma Srikanth <84093378+karishmas26@users.noreply.github.com>
2024-04-10 14:49:15 +0200

    pushing up starter files

---
+++ Scripts/test_script.R
@@ -0,0 +1,27 @@
+# AUTHOR:   K. Srikanth | USAID
+# PURPOSE:  
+# REF ID:   c0fee232 
+# LICENSE:  MIT
+# DATE:     2024-04-10
+# UPDATED: 
+
+# DEPENDENCIES ------------------------------------------------------------
+  
+  sipack
+
+# GLOBAL VARIABLES --------------------------------------------------------
+  
+  # SI specific paths/functions  
+    load_secrets()
+
+  # Grab metadata
+    get_metadata(filepath) 
+  
+  ref_id <- "c0fee232"
+
+# IMPORT ------------------------------------------------------------------
+  
+  return_data
+  msd
+
+# MUNGE -------------------------------------------------------------------
